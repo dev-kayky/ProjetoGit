5c9f5082569ceff4e7dea95390f7ebbf34735162
Felipe Mendes <felipemendesso52222@gmail.com>
2025-06-09 05:25:09 -0300

    Alterações de interface, correções de problemas de conflito entre grip e pack utilizando frame e definição de entrys e combobox em função do get

--- moedas.py
+++ moedas.py
@@ -9,8 +9,8 @@ TAXAS = {
 }
 
 def converter():
-    moeda_inicial = combo.inicial.get()
-    moeda_final = combo.final.get()
+    moeda_inicial = combo_inicial.get()
+    moeda_final = combo_final.get()
     valor_str = entrada_valor.get()
 
     if not valor_str:
@@ -22,10 +22,26 @@ janela.title("Conversor de Moedas")
 janela.configure(bg="black")
 label_inicial = tk.Label(janela, text="Conversor de Moedas - Início", fg="white", bg="black")
 label_inicial.pack(padx=20, pady=20)
+frame = ttk.Frame(janela)
+frame.pack(padx=10, pady=10)
 moedas = list(TAXAS.keys())
 moeda_inicial_var = tk.StringVar(janela)
 moeda_final_var = tk.StringVar(janela)
 valor_entrada = tk.Entry(janela)
 resultado = tk.StringVar(janela)
+label_moeda1 = ttk.Label(frame, text="Moeda à ser convertida:")
+label_moeda1.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+combo_inicial = ttk.Combobox(frame, textvariable=moeda_inicial_var, values=moedas, width=10)
+combo_inicial.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+combo_inicial.set("BRL")
+label_moeda2 = ttk.Label(frame, text="Moeda para qual será convertida:")
+label_moeda2.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+combo_final = ttk.Combobox(frame, textvariable=moeda_final_var, values=moedas, width=10)
+combo_final.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+combo_final.set("USD")
+label_valor = ttk.Label(frame, text="Valor para a conversão:")
+label_valor.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+entrada_valor = ttk.Entry(frame, width=15)
+entrada_valor.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
 
 janela.mainloop()
